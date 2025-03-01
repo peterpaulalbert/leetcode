c4eb677c832e82420ed49403f0bc99027ca32853
Peter Paul Albert Irlandez <irlandez@gmail.com>
2025-03-01 22:07:01 +0800

    Solved category 'Basic Array Transformations' of '30 Days of JavaScript'

---
+++ Study Plan/30 Days Challenge/30 Days of JavaScript/02 - Basic Array Transformations/01 - #2635. Apply Transform Over Each Element in Array/app.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function (arr, fn) {
+	let output = new Array(arr.length);
+	for (let i = 0; i < arr.length; i++) {
+		output[i] = fn(arr[i], i);
+	}
+	return output;
+};
